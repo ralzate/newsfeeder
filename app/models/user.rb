159ad895ecbe8807129de9d0ed225d0cdbbd85ad
tkueny12 <thomaspkueny@gmail.com>
2016-05-04 17:09:56 -0400

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ActiveRecord::Base
+    has_many :stories
+
+    class << self
+        def from_omniauth(auth)
+            user = User.find_or_initialize_by(uid: auth['uid'])
+            user.name = auth['info']['name']
+            user.avatar_url = auth['info']['image']
+            user.save!
+            user
+        end
+        private
+
+        def current_user
+            @current_user ||= User.find_by(id: session[:user_id])
+        end
+    end
+end
